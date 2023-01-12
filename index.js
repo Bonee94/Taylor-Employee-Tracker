ec5c8622005c99b1b0652d942741573619fb7fa9
Bonee94 <bonee94@yahoo.com>
2023-01-12 15:50:03 -0600

    Added switch case statments to handle the 'View' selections and restart the prompt

--- index.js
+++ index.js
@@ -1,21 +1,52 @@
-const inquirer = require('inquirer');
-
+const inquirer = require("inquirer");
+const dbQuery = require("./server");
 
 const toDoPrompt = () => {
-inquirer
-.prompt([
-    {
-        type: 'list',
-        message: 'What would you like to do?',
-        name: 'selectedToDo',
-        choices: ['View all departments', 'View all Roles', 'View all employees', 'Add a department', 'Add a role', 'Add a employee', 'Update an employee role'],
-    }
-])
-.then(data => {
-    console.log(data.selectedToDo);
-})}
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What would you like to do?",
+        name: "selectedToDo",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Add a department",
+          "Add a role",
+          "Add a employee",
+          "Update an employee role",
+        ],
+      },
+    ])
+    .then((data) => {
+      //console.log(data.selectedToDo);
+      console.log("\n");
 
+      switch (data.selectedToDo) {
+        case "View all departments":
+          dbQuery.allDept();
+          timedPrompt();
+          break;
+        case "View all roles":
+          dbQuery.allRoles();
+          timedPrompt();
+          break;
+        case "View all employees":
+          dbQuery.allEmployees();
+          timedPrompt();
+          break;
+      }
+    });
+};
 
+// This function makes the question prompt be delayed so that the console
+// can log the returned results correctly without overlapping
+const timedPrompt = () => {
+  setTimeout(() => {
+    toDoPrompt();
+  }, 500);
+};
 
-
+// Initializes prompt
 toDoPrompt();
